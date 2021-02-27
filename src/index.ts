b1859754ae9c68e1a05bb89e6c9b40fd1a4e7818
이기현 <dave@igihyeon-ui-MacBookAir.local>
2021-02-27 17:26:06 +0900

    Subscriptions

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,17 @@
-import { count } from "console";
 import { Action, createStore, Store } from "redux";
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+const add: HTMLElement | null = document.getElementById("add");
+const minus: HTMLElement | null = document.getElementById("minus");
+const number: any = document.querySelector("span");
 
 // Reducer
 const countModifier = (state = 0, action: Action): number => {
   if (action.type === "ADD") {
     // add 1 to State when action is Add
-    return state++;
+    state++;
   } else if (action.type === "MINUS") {
     // minus 1 to State when action is Minus
-    return state--;
+    state--;
   }
   return state;
 };
@@ -20,9 +19,18 @@ const countModifier = (state = 0, action: Action): number => {
 // Create Store
 const countStore: Store = createStore(countModifier);
 
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "MINUS" });
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
 
-console.log(countStore.getState());
+countStore.subscribe(onChange);
+
+// add 1 when button is clicked
+add?.addEventListener("click", () => {
+  countStore.dispatch({ type: "ADD" });
+});
+
+// minus 1 when button is clicked
+minus?.addEventListener("click", () => {
+  countStore.dispatch({ type: "MINUS" });
+});
