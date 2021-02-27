45e6feb399e2e312a058c3b7e409bcd2620af928
이기현 <dave@igihyeon-ui-MacBookAir.local>
2021-02-27 17:14:15 +0900

    Actions

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,28 @@
-import { createStore, Store } from "redux";
+import { count } from "console";
+import { Action, createStore, Store } from "redux";
 
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
 // Reducer
-const countModifier = (state = 0): number => {
+const countModifier = (state = 0, action: Action): number => {
+  if (action.type === "ADD") {
+    // add 1 to State when action is Add
+    return state++;
+  } else if (action.type === "MINUS") {
+    // minus 1 to State when action is Minus
+    return state--;
+  }
   return state;
 };
 
 // Create Store
 const countStore: Store = createStore(countModifier);
 
-countStore.getState();
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
+
+console.log(countStore.getState());
