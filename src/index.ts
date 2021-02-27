231dee53620a51b3d59badb572a54c7d59a0bb4f
이기현 <dave@igihyeon-ui-MacBookAir.local>
2021-02-27 20:41:55 +0900

    Delete To Do

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,25 @@
 import { AnyAction, createStore, Reducer, Store } from "redux";
+import { ToDo } from "./react-app-env";
 
 const form = document.querySelector("form");
 const input: any = document.querySelector("input");
-const ul = document.querySelector("ul");
+const ul: any = document.querySelector("ul");
 
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
+const addToDo = (text: string, id: string) => {
+  return { type: ADD_TODO, text, id };
+};
+
+const deleteToDo = (id: string) => {
+  return { type: DELETE_TODO, id };
+};
+
 const reducer: Reducer = (state = [], { type, text, id }: AnyAction) => {
   switch (type) {
     case ADD_TODO:
-      return [...state, { text, id }];
+      return [{ text, id }, ...state];
     case DELETE_TODO:
       return [];
     default:
@@ -20,14 +29,38 @@ const reducer: Reducer = (state = [], { type, text, id }: AnyAction) => {
 
 const store: Store = createStore(reducer);
 
-store.subscribe(() => console.log(store.getState()));
+const dispatchDeleteToDo = (e: any) => {
+  const id = e.target.parentNode.id;
+  store.dispatch(deleteToDo(id));
+};
+
+const dispatchAddToDo = (text: string, id: string) => {
+  store.dispatch(addToDo(text, id));
+};
+
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = "";
+  toDos.forEach((toDo: ToDo) => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "Delete";
+    btn.addEventListener("click", dispatchDeleteToDo);
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    li.appendChild(btn);
+    ul?.appendChild(li);
+  });
+};
+
+store.subscribe(paintToDos);
 
 const onSubmit = (e: Event) => {
   e.preventDefault();
   const toDo = input.value;
   const id = store.getState().length;
   input.value = "";
-  store.dispatch({ type: ADD_TODO, text: toDo, id });
+  dispatchAddToDo(toDo, id);
 };
 
 form?.addEventListener("submit", onSubmit);
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="react-scripts" />
+
+import { type } from "os";
+
+export type ToDo = {
+  text: string;
+  id: string;
+};
